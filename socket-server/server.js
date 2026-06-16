321c0dbb742f5d02cccb19907957a7cb95bf7590
Shubham Bansal <sm325642@gmail.com>
2026-06-16 22:38:54 +0530

    final

--- socket-server/server.js
+++ socket-server/server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:3000",
-      "https://stock-trading-ruby.vercel.app/stocks"
+      "https://stock-trading-ruby.vercel.app/"
     ], // Next.js frontend
   },
 });
